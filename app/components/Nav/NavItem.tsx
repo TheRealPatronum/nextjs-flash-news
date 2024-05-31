d44f7edd012312f54aa351f62df7ae9983eecf88
Ragnar Östh <ragnar.osth@afry.com>
2024-05-31 13:50:12 +0200

    added mock fake-server

--- app/components/Nav/NavItem.tsx
+++ app/components/Nav/NavItem.tsx
@@ -1,11 +1,17 @@
+'use client'
 import Image, { StaticImageData } from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export function NavItem(p: { category: string; alt: string; src: StaticImageData }) {
+  const pathname = usePathname()
+  const isActive = '/articles/category/' + p.category === pathname
   return (
     <Link
       href={'/articles/category/' + p.category}
-      className="flex items-center h-12 gap-2 p-3 hover:bg-slate-100 transform transition hover:scale-105 rounded-xl"
+      className={`flex items-center h-12 gap-2 p-3 hover:bg-slate-100 transform transition hover:scale-105 rounded-xl
+      ${isActive && 'bg-orange-50'}
+      `}
     >
       <div className="flex justify-center items-center rounded-full border border-[#BBC2CC] w-10 h-10">
         <Image src={p.src} alt={p.alt} className="w-5 h-5" />
