46ec15792234f6fd784c3db944a6a8b93df7a773
Ragnar Östh <ragnar.osth@afry.com>
2024-06-03 11:31:39 +0200

    Added Article card

--- app/(pages)/articles/category/[category]/page.tsx
+++ app/(pages)/articles/category/[category]/page.tsx
@@ -1,7 +1,11 @@
 import { CATEGORIES_ITEMS } from '@/app/components/constant'
+import { ArticleCategory } from '@/app/types/article-type'
 import Image from 'next/image'
 
-export default function CategoryDetailPage(p: { params: { category: keyof typeof CATEGORIES_ITEMS } }) {
+export default function CategoryDetailPage(p: {
+  params: { category: ArticleCategory; date: string }
+  searchParams: { date: string }
+}) {
   const categoryItem = CATEGORIES_ITEMS[p.params.category]
   return (
     <div className="flex items-center space-x-4">
