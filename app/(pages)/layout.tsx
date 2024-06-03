5abd8679334cbd0ab00bce1571096b29b28c73bd
Ragnar Östh <ragnar.osth@afry.com>
2024-06-03 14:22:29 +0200

    adding metadata

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,8 +1,14 @@
 import { Nav } from '@/app/components/Nav/Nav'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Nextjs flash news',
+  description: 'Get the latest flash news',
+}
 export default function RootLayout({
   children,
 }: Readonly<{
