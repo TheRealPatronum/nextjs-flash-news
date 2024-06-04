9815e1a2987a5d872748bff038c9fdbc0565c918
Ragnar Östh <ragnar.osth@afry.com>
2024-06-04 11:12:07 +0200

    Refactor layout

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,25 +1,13 @@
 import { Nav } from '@/app/components/Nav/Nav'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { Metadata } from 'next'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Nextjs flash news',
-  description: 'Get the latest flash news',
-}
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} flex p-8`}>
-        <Nav />
-        <div className="px-8 mt-16">{children}</div>
-      </body>
-    </html>
+    <div className="flex p-8">
+      <Nav />
+      <div className="mt-16 pl-8 w-full">{children}</div>
+    </div>
   )
 }
