0625faaebe26744edd25ef1d0d5c36e5ec813dea
Ragnar Östh <ragnar.osth@afry.com>
2024-06-04 17:20:48 +0200

    Adding Clock with no SSR

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,13 +1,23 @@
 import { Nav } from '@/app/components/Nav/Nav'
+import Clock from '../components/Clock/Clock'
+import dynamic from 'next/dynamic'
+
+const ClockNoSSR = dynamic(() => import('@/app/components/Clock/Clock'), {
+  ssr: false,
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div className="flex p-8">
-      <Nav />
-      <div className="mt-16 pl-8 w-full">{children}</div>
+    <div className="p-8">
+      <ClockNoSSR />
+      <div className="flex">
+        <Nav />
+        <div className="mt-16 pl-8 w-full">{children}</div>
+      </div>
     </div>
   )
 }
