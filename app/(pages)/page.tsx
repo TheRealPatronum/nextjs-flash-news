7f347e811f019b651a160fe5db721deb69b2ebd9
Ragnar Östh <ragnar.osth@afry.com>
2024-06-04 17:54:26 +0200

    added crypto news for bitcoin

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,9 +1,11 @@
+import { CryptoNews } from '../components/CryptoNews/CryptoNews'
 import { LatestNews } from '../components/LatestNews/LatestNews'
 
 export default function IndexPage() {
   return (
-    <div>
+    <div className="flex justify-between">
       <LatestNews />
+      <CryptoNews />
     </div>
   )
 }
