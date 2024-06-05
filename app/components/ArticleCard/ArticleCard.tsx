9f37f50ff1e59c7d01165072bf2722385c054369
Ragnar Östh <ragnar.osth@afry.com>
2024-06-05 11:47:16 +0200

    Removed Hight setting on picture

--- app/components/ArticleCard/ArticleCard.tsx
+++ app/components/ArticleCard/ArticleCard.tsx
@@ -16,21 +16,29 @@ export function ArticleCard(p: { article: Article }) {
         {/* Icon and category*/}
         <div className="flex items-center gap-2">
           <div className="flex w-10 h-10 justify-center items-center border border-slate-300 rounded-full">
-            <Image className="w-5 h-5" src={CATEGORIES_ITEMS[p.article.category[0]].src} alt="Icon for category" />
+            <Image
+              className="w-5 h-5"
+              src={CATEGORIES_ITEMS[p.article.category[0]].src}
+              alt="Icon for category"
+            />
           </div>
           <div className="font-bold text-md">{p.article.category}</div>
         </div>
         {/* Date */}
-        <div className="mt-2 text-sm text-gray-400">Published : {new Date(p.article.pubDate).toDateString()}</div>
+        <div className="mt-2 text-sm text-gray-400">
+          Published : {new Date(p.article.pubDate).toDateString()}
+        </div>
       </div>
 
       {/* Body*/}
       <div className="space-y-2">
         {/* Title*/}
-        <div className="font-semibold text-xl line-clamp-2 h-14">{p.article.title}</div>
+        <div className="font-semibold text-xl line-clamp-2 h-14">
+          {p.article.title}
+        </div>
         {/* Article image*/}
         <ImageWithFallback
-          className="h-40 rounded-lg"
+          className="rounded-lg"
           height={200}
           width={300}
           src={p.article.image_url || defaultNewsPng}
--- app/components/ImageWithFallback/ImageWithFallback.tsx
+++ app/components/ImageWithFallback/ImageWithFallback.tsx
@@ -2,7 +2,11 @@
 import Image, { ImageProps, StaticImageData } from 'next/image'
 import { useState } from 'react'
 
-export function ImageWithFallback({ fallback, src, ...p }: ImageProps & { fallback: StaticImageData }) {
+export function ImageWithFallback({
+  fallback,
+  src,
+  ...p
+}: ImageProps & { fallback: StaticImageData }) {
   const [imgSrc, setImgSrc] = useState(src)
   return (
     <Image
